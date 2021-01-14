7ec7a9d4a5f058bb44f6b80e830873b91f91456a
CJ Lazell <cjlazell@gmail.com>
2021-01-14 16:05:04 -0600

    removed column encryption

--- db/migrate/20201218212126_devise_create_users.rb
+++ db/migrate/20201218212126_devise_create_users.rb
@@ -4,6 +4,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table(:users, id: :uuid) do |t|
       ## Database authenticatable
+      t.string(:first_name, null: false)
+      t.string(:last_name, null: false)
       t.string(:email,              null: false, default: "")
       t.string(:encrypted_password, null: false, default: "")
 
@@ -31,10 +33,15 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       t.integer(:failed_attempts, default: 0, null: false) # Only if lock strategy is :failed_attempts
       t.string(:unlock_token) # Only if unlock strategy is :email or :both
       t.datetime(:locked_at)
+      t.string(:time_zone)
 
       t.timestamps(null: false)
     end
 
+    add_index(:users, :first_name)
+    add_index(:users, :last_name)
+    add_index(:users, [:first_name, :last_name])
+    add_index(:users, :time_zone)
     add_index(:users, :email,                unique: true)
     add_index(:users, :reset_password_token, unique: true)
     add_index(:users, :confirmation_token,   unique: true)
